13aac6dfcb7ec7842a618a0534d20061af7137ca
vitis22 <151199365+vitis22@users.noreply.github.com>
2023-11-20 22:12:38 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const questions = [
         options: ["Medvěd polární", "Krokodýl", "Tygr", "Slon"],
         answer: 0,
         difficulty: "lehká",
-        image: "images/panda.jpeg"
+        image: "panda.jpeg"
     },
     {
         question: "Jaký pták neumí létat?",
@@ -112,17 +112,6 @@ function loadQuestion() {
 function checkAnswer(event) {
     const selectedOption = event.target;
     const selectedOptionIndex = Array.from(options).indexOf(selectedOption);
-    const correctOptionIndex = questions[currentQuestion].answer;
-
-    for (let i = 0; i < options.length; i++) {
-        options[i].removeEventListener("click", checkAnswer);
-
-        if (i === correctOptionIndex) {
-            options[i].classList.add("correct");
-        } else if (i === selectedOptionIndex) {
-            options[i].classList.add("incorrect");
-        }
-    }
 
     if (selectedOptionIndex === questions[currentQuestion].answer) {
         score++;
@@ -138,20 +127,8 @@ function checkAnswer(event) {
     nextButton.style.display = "block";
 }
 
-
-
 function showResult() {
-    // Počet správných odpovědí pro vybranou obtížnost
-    const spravneOdpovedi = questions.reduce((pocet, otazka) => {
-        if (otazka.difficulty === selectedDifficulty && otazka.answer === questions.indexOf(otazka.options.find((opt, idx) => idx === otazka.answer))) {
-            return pocet + 1;
-        }
-        return pocet;
-    }, 0);
-
-    // Zobrazení počtu správných odpovědí pro vybranou obtížnost
-    questionElement.textContent = `Váš výsledek: ${spravneOdpovedi} správných odpovědí z ${questions.filter(otazka => otazka.difficulty === selectedDifficulty).length}.`;
-
+    questionElement.textContent = `Váš výsledek: ${score} správných odpovědí z ${questions.length}.`;
     options.forEach(option => option.style.display = "none");
     nextButton.style.display = "none";
 }
