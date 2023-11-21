7fa66290f8068b4e4a34a19bca003b8e1abb4e33
vitis22 <151199365+vitis22@users.noreply.github.com>
2023-11-21 23:27:28 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,17 +1,156 @@
 const questions = [
     {
-        question: "Které zvíře žije v Antarktidě?",
+        question: "Co je to za zvíře?",
+        options: ["Medvěd polární", "Panda", "Tygr", "Slon"],
+        answer: 1,
+        difficulty: "lehká",
+        image: "panda.jpeg"
+    },
+    {
+        question: "Co je to za zvíře?",
+        options: ["Medvěd polární", "Krokodýl", "Tygr", "Slon"],
+        answer: 2,
+        difficulty: "lehká",
+        image: "tygr.jpeg"
+    },
+    {
+        question: "Co je to za zvíře?",
+        options: ["Slon africký", "Tygr bengálský", "Lev africký", "Vlk obecný"],
+        answer: 1,
+        difficulty: "lehká",
+        image: "slon.jpeg"
+    },
+    {
+        question: "Co je to za zvíře?",
+        options: ["Lev africký", "Osel domácí", "Kočka divoká", "Veverka"],
+        answer: 1,
+        difficulty: "lehká",
+        image: "lev.jpeg"
+    },    
+    {
+        question: "Co je to za zvíře?",
+        options: ["Vlk obecný", "Nosorožec bílý", "Koala", "Žirafa"],
+        answer: 1,
+        difficulty: "lehká",
+        image: "vlk.jpeg"
+    },    
+    {
+        question: "Co je to za zvíře?",
+        options: ["Panda obrovská", "Kočka domácí", "Delfín skákavý", "Krokodýl nilský"],
+        answer: 1,
+        difficulty: "lehká",
+        image: "panda.jpeg"
+    },    
+    {
+        question: "Co je to za zvíře?",
+        options: ["Panda obrovská", "Kočka domácí", "Delfín skákavý", "Krokodýl nilský"],
+        answer: 1,
+        difficulty: "lehká",
+        image: "panda.jpeg"
+    },
+    {
+        question: "Co je to za zvíře?",
+        options: ["Koala", "Veverka", "Osel domácí", "Klokánek rudý"],
+        answer: 1,
+        difficulty: "lehká",
+        image: "koala.jpeg"
+    },
+    {
+        question: "9 Co je to za zvíře?",
         options: ["Medvěd polární", "Krokodýl", "Tygr", "Slon"],
         answer: 0,
         difficulty: "lehká",
-        image: "images/panda.jpeg"
+        image: "panda.jpeg"
     },
     {
-        question: "Jaký pták neumí létat?",
+        question: "10 Co je to za zvíře?",
+        options: ["Medvěd polární", "Krokodýl", "Tygr", "Slon"],
+        answer: 0,
+        difficulty: "lehká",
+        image: "panda.jpeg"
+    },
+    {
+        question: "11 Co je to za zvíře?",
+        options: ["Medvěd polární", "Krokodýl", "Tygr", "Slon"],
+        answer: 0,
+        difficulty: "lehká",
+        image: "panda.jpeg"
+    },
+    {
+        question: "1 Jaký pták neumí létat?",
         options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
         answer: 0,
         difficulty: "střední",
         image: "tygr.jpeg"
+    },
+    {
+        question: "2 Jaký pták neumí létat?",
+        options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
+        answer: 0,
+        difficulty: "střední",
+        image: "tygr.jpeg"
+    },
+    {
+        question: "3 Jaký pták neumí létat?",
+        options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
+        answer: 0,
+        difficulty: "střední",
+        image: "tygr.jpeg"
+    },
+    {
+        question: "4 Jaký pták neumí létat?",
+        options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
+        answer: 0,
+        difficulty: "střední",
+        image: "tygr.jpeg"
+    },
+    {
+        question: "5 Jaký pták neumí létat?",
+        options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
+        answer: 0,
+        difficulty: "střední",
+        image: "tygr.jpeg"
+    },
+    {
+        question: "6 Jaký pták neumí létat?",
+        options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
+        answer: 0,
+        difficulty: "střední",
+        image: "tygr.jpeg"
+    },
+    {
+        question: "7 Jaký pták neumí létat?",
+        options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
+        answer: 0,
+        difficulty: "střední",
+        image: "tygr.jpeg"
+    },
+
+    {
+        question: "8 Jaký pták neumí létat?",
+        options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
+        answer: 0,
+        difficulty: "střední",
+        image: "tygr.jpeg"
+    },
+    {
+        question: "9 Jaký pták neumí létat?",
+        options: ["Pštros", "Kondor", "Vlaštovka", "Sokol"],
+        answer: 0,
+        difficulty: "střední",
+        image: "tygr.jpeg"
+    },
+    {
+        question: "Který druh žraloka je největší?",
+        options: ["Žralok bílý", "Žralok veliký", "Žralok obrovský", "Žralok makoa"],
+        answer: 2,
+        difficulty: "střední"
+    },  
+    {
+        question: "Které zvíře je symbolem Austrálie?",
+        options: ["Klokan", "Koala", "Emu", "Platypus"],
+        answer: 0,
+        difficulty: "střední"
     },
     {
         question: "Kolik noh má pavouk?",
@@ -25,20 +164,6 @@ const questions = [
         answer: 1,
         difficulty: "těžká"
     },
-    
-    {
-        question: "Který druh žraloka je největší?",
-        options: ["Žralok bílý", "Žralok veliký", "Žralok obrovský", "Žralok makoa"],
-        answer: 2,
-        difficulty: "střední"
-    },
-    {
-        question: "Které zvíře je symbolem Austrálie?",
-        options: ["Klokan", "Koala", "Emu", "Platypus"],
-        answer: 0,
-        difficulty: "střední"
-    },
-    
     {
         question: "Kolik druhů želv je známo?",
         options: ["7", "250", "360", "500+"],
@@ -47,6 +172,54 @@ const questions = [
     },
     {
         question: "Který druh chobotnice je schopný měnit barvu?",
+        options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
+        answer: 1,
+        difficulty: "těžká"
+    },
+    {
+        question: "4 Který druh chobotnice je schopný měnit barvu?",
+        options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
+        answer: 1,
+        difficulty: "těžká"
+    },
+    {
+        question: "5 Který druh chobotnice je schopný měnit barvu?",
+        options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
+        answer: 1,
+        difficulty: "těžká"
+    },
+    {
+        question: "6 Který druh chobotnice je schopný měnit barvu?",
+        options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
+        answer: 1,
+        difficulty: "těžká"
+    },
+    {
+        question: "7 Který druh chobotnice je schopný měnit barvu?",
+        options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
+        answer: 1,
+        difficulty: "těžká"
+    },
+    {
+        question: "8 Který druh chobotnice je schopný měnit barvu?",
+        options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
+        answer: 1,
+        difficulty: "těžká"
+    },
+    {
+        question: "9 Který druh chobotnice je schopný měnit barvu?",
+        options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
+        answer: 1,
+        difficulty: "těžká"
+    },
+    {
+        question: "10 Který druh chobotnice je schopný měnit barvu?",
+        options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
+        answer: 1,
+        difficulty: "těžká"
+    },
+    {
+        question: "11 Který druh chobotnice je schopný měnit barvu?",
         options: ["Chobotnice velká", "Kohoutková krakatice", "Chobotnice globová", "Chobotnice mimikri"],
         answer: 1,
         difficulty: "těžká"
@@ -82,36 +255,42 @@ function resetQuiz() {
 
 function loadQuestion() {
     if (currentQuestion < questions.length) {
-        const currentDifficulty = questions[currentQuestion].difficulty;
+        const questionData = questions[currentQuestion];
+        const currentDifficulty = questionData.difficulty;
 
         if (currentDifficulty === selectedDifficulty) {
-            questionElement.textContent = `(${currentDifficulty}) ${questions[currentQuestion].question}`;
-
-             const questionImage = document.getElementById("question-image");
-            questionImage.src = questions[currentQuestion].image;
-            questionImage.style.display = "block";
-
-            const currentOptions = questions[currentQuestion].options;
-            for (let i = 0; i < options.length; i++) {
-                if (i < currentOptions.length) {
-                    options[i].textContent = currentOptions[i];
-                    options[i].classList.remove("correct", "incorrect");
-                    options[i].addEventListener("click", checkAnswer);
-                    options[i].style.display = "block"; 
-                } else {
-                    options[i].style.display = "none"; 
-                }
-            }
+            updateQuestion(questionData);
         } else {
             currentQuestion++;
-            loadQuestion();
-            return;
+            loadQuestion(); // Oprava: Zavoláme loadQuestion() znovu, pokud aktuální otázka nemá správnou obtížnost
         }
     } else {
         showResult();
     }
 }
 
+function updateQuestion(questionData) {
+    questionElement.textContent = `(${questionData.difficulty}) ${questionData.question}`;
+
+    const questionImage = document.getElementById("question-image");
+    questionImage.src = `images/${questionData.image}`; // Oprava: Přidáme cestu k adresáři images
+    questionImage.style.display = "block";
+
+    updateOptions(questionData.options);
+}
+
+function updateOptions(currentOptions) {
+    for (let i = 0; i < options.length; i++) {
+        if (i < currentOptions.length) {
+            options[i].textContent = currentOptions[i];
+            options[i].classList.remove("correct", "incorrect");
+            options[i].addEventListener("click", checkAnswer);
+            options[i].style.display = "block"; 
+        } else {
+            options[i].style.display = "none"; 
+        }
+    }
+}
 
 function checkAnswer(event) {
     const selectedOption = event.target;
